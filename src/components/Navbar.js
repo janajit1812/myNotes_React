5b8fbc2efb524012caf861535762b24bd765b5ff
Janajit <janajitdatta@gmail.com>
2023-09-22 16:30:01 +0530

    Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,12 +29,12 @@ const Navbar = () => {
                                 <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link>
                             </li>
                         </ul>
-                        
+
                          {!localStorage.getItem('token') ? <form className="d-flex" role="search">
                             <Link className="btn btn-success mx-2" to="/login" role="button">Login</Link>
                             <Link className="btn btn-success" to="/signup" role="button">Sign Up</Link>
                         </form> : <form className="d-flex" role="search">
-                            <Link className="btn btn-sm btn-success mx-2" to="/userdetail" role="button"><i class="fa-regular fa-user"></i></Link>
+                            <Link className="btn btn-sm btn-success mx-2" to="/userdetail" role="button"><i className="fa-regular fa-user"></i></Link>
                             <button onClick={handleLogout} type="button" className="btn btn-sm btn-danger">Log Out</button>
                         </form>}
                     </div>
